7265920af81ebed6885a98a9f7a5bdfc95794a7a
Tung Hoang <vinhlieu1942@gmail.com>
2024-04-25 11:04:29 +0700

    update plugin ( so much )

---
+++ build/4-wheel-differential-car-ros/gazebo_plugins/CMakeFiles/gazebo_ros_video.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/tung/4-wheel-differential-car-ros/devel/lib/libgazebo_ros_video.pdb"
+  "/home/tung/4-wheel-differential-car-ros/devel/lib/libgazebo_ros_video.so"
+  "CMakeFiles/gazebo_ros_video.dir/src/gazebo_ros_video.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gazebo_ros_video.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/4-wheel-differential-car-ros/gazebo_plugins/CMakeFiles/hokuyo_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/tung/4-wheel-differential-car-ros/devel/lib/gazebo_plugins/hokuyo_node"
+  "/home/tung/4-wheel-differential-car-ros/devel/lib/gazebo_plugins/hokuyo_node.pdb"
+  "CMakeFiles/hokuyo_node.dir/src/hokuyo_node.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hokuyo_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
