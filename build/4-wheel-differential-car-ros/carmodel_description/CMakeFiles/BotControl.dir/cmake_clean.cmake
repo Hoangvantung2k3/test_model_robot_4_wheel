987017ec46cec154c3b9a7c8fba6629e4dac3eef
Tung Hoang <vinhlieu1942@gmail.com>
2024-05-24 16:43:58 +0700

    build pid

---
+++ build/4-wheel-differential-car-ros/carmodel_description/CMakeFiles/BotControl.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/tung/4-wheel-differential-car-ros/devel/lib/carmodel_description/BotControl"
+  "/home/tung/4-wheel-differential-car-ros/devel/lib/carmodel_description/BotControl.pdb"
+  "CMakeFiles/BotControl.dir/src/BotControl.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BotControl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
